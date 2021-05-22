47aff369a966cb7dcb7f0bdf6276ebb7daadeba1
sirkenedy <owoputokehinde@gmail.com>
2021-05-22 11:42:36 +0100

    handle payment exception

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -24,25 +24,31 @@ class PaymentController extends Controller
 
     public function handleGatewayCallback()
     {
-        //db transaction 
-        DB::beginTransaction();
-        $paymentDetails = Paystack::getPaymentData();
-        // dd($paymentDetails);
-        //update entry status
-        $data = $paymentDetails['data'];
-        $entry = Entry::where('id', $data['metadata']['entry_id'])->with(['orders', 'user'])->first();
-        $entry->update(['status' => 1]);
-        
+        try{
+            
+            //db transaction 
+            DB::beginTransaction();
+            $paymentDetails = Paystack::getPaymentData();
+            // dd($paymentDetails);
+            //update entry status
+            $data = $paymentDetails['data'];
+            $entry = Entry::where('id', $data['metadata']['entry_id'])->with(['orders', 'user'])->first();
+            $entry->update(['status' => 1]);
+            
 
-        //update transaction table
-        $entry->transactions()->save(new Transaction(['amount_paid'=> $data['amount'], 'reference_no'=>$data['reference'], 'status'=> $data['status'] == "success" ? "1" : "0"]));
+            //update transaction table
+            $entry->transactions()->save(new Transaction(['amount_paid'=> $data['amount'], 'reference_no'=>$data['reference'], 'status'=> $data['status'] == "success" ? "1" : "0"]));
 
-        //dispatch event to dispatch email to admin and user
+            //dispatch event to dispatch email to admin and user
 
-        event(new EntryProcessed($entry));
-        
-        DB::commit();
-        // return $entry;
-        return redirect('/entries')->with(['success'=> 'Payment successful. Summary of your order has been received and a copy has been sent to your Mail']);
+            event(new EntryProcessed($entry));
+            
+            DB::commit();
+            // return $entry;
+            return redirect('/entries')->with(['success'=> 'Payment successful. Summary of your order has been received and a copy has been sent to your Mail']);
+        }catch(\Exception $e) {
+            
+            return redirect('/entries')->with(['success'=> $e->getMessage()]);
+        }  
     }
 }
